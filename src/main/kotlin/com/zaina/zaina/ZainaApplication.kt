0ef23dcb0e39d443c353885e675310fbd8747dde
azmenezi <azmenezi@gmail.com>
2025-06-15 18:46:04 +0300

    .

---
+++ src/main/kotlin/com/zaina/zaina/ZainaApplication.kt
@@ -0,0 +1,11 @@
+package com.zaina.zaina
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZainaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZainaApplication>(*args)
+}
---
+++ src/test/kotlin/com/zaina/zaina/ZainaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zaina.zaina
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZainaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
